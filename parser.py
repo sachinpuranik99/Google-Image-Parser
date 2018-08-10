8c8c341625047303b0ab5ae64de2deefbd2f0175
Sachin Puranik <32604540+sachinpuranik99@users.noreply.github.com>
2018-08-09 18:17:20 -0700

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import json
+import os
+import urllib2
+import argparse
+import sys
+
+
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+	print "ERROR: Please provide one celebrity name at a time in double quotes as the first argument"
+	sys.exit()
+elif not isinstance(sys.argv[1], basestring):
+	print "Please provide name in quotes as a string"
+	sys.exit()
+else:
+	print 
+
+	
+searchterm = sys.argv[1] # will also be the name of the folder
+url = "https://www.google.co.in/search?q="+searchterm+"&source=lnms&tbm=isch"
+
+# NEED TO DOWNLOAD CHROMEDRIVER, insert path to chromedriver inside parentheses in following line
+browser = webdriver.Chrome()
+browser.get(url)
+header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+counter = 0
+succounter = 0
+
+if not os.path.exists(searchterm):
+    os.mkdir(searchterm)
+
+for _ in range(400):
+    browser.execute_script("window.scrollBy(0,10000)")
+
+for x in browser.find_elements_by_xpath('//div[contains(@class,"rg_meta")]'):
+    counter = counter + 1
+    print "Total Count:", counter
+    print "Succsessful Count:", succounter
+    print "URL:",json.loads(x.get_attribute('innerHTML'))["ou"]
+
+    img = json.loads(x.get_attribute('innerHTML'))["ou"]
+    imgtype = json.loads(x.get_attribute('innerHTML'))["ity"]
+    try:
+        req = urllib2.Request(img, headers={'User-Agent': header})
+        raw_img = urllib2.urlopen(req).read()
+        if("jpg" == imgtype):
+            File = open(os.path.join(searchterm ,str(counter) + "." + imgtype), "wb")
+            File.write(raw_img)
+            succounter = succounter + 1
+            File.close()
+            print imgtype, File
+    except:
+            print "can't get img"
+
+print succounter, "pictures succesfully downloaded"
+browser.close()
